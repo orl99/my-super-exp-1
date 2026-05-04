dce864d8d5fcc074666582ddcd8e13a512e4a373
Orly <oeg@cbord.com>
2026-05-03 22:17:48 -0600

    Image picker feature

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,40 @@
 import { StyleSheet, View } from "react-native";
 
+import Button from '@/components/Button';
 import ImageViewer from "@/components/ImageViewer";
 const PlaceholderImage = require("@/assets/images/background-image.png");
 
+import * as ImagePicker from 'expo-image-picker';
+
 export default function Index() {
+
+
+    // ImagePicker Init
+    const pickerImageAsync = async () => {
+        const result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ['images', 'livePhotos', 'videos'],
+            allowsEditing: true,
+            quality: 1
+        });
+
+        if (!result.canceled) {
+            console.log('Result', result);
+        } else {
+            alert('You did not select any image');
+        }
+    }
+
+
     return (
         <View
             style={styles.container}
         >
             <View style={styles.imageContainer}>
                 <ImageViewer imageSource={PlaceholderImage} />
+            </View>
+            <View style={styles.footerContainer}>
+                <Button theme="primary" label="Choose a photo" onPress={pickerImageAsync} />
+                <Button label="Use this photo" />
             </View>
             {/* <Link style={styles.button} href="./about">Go to About</Link> */}
         </View>
@@ -39,5 +64,10 @@ const styles = StyleSheet.create({
         textDecorationLine: 'underline',
         color: '#fff',
     },
+    footerContainer: {
+        flex: 1 / 3,
+        alignItems: 'center',
+    },
+
 });
 
--- components/ImageViewer.tsx
+++ components/ImageViewer.tsx
@@ -2,11 +2,13 @@ import { Image } from 'expo-image';
 import { ImageSourcePropType, StyleSheet } from 'react-native';
 
 type Props = {
-    imageSource: ImageSourcePropType
+    imageSource: ImageSourcePropType;
+    selectedImage?: string;
 }
 
-export default function ImageViewer({ imageSource }: Props) {
-    return <Image source={imageSource} style={styles.image} />
+export default function ImageViewer({ imageSource, selectedImage }: Props) {
+    const imgSource = selectedImage ? { uri: selectedImage } : imageSource;
+    return <Image source={imgSource} style={styles.image} />
 }
 
 const styles = StyleSheet.create({
